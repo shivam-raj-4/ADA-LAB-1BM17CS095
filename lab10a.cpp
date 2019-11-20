69b8852d036b0c258435e54356b626ea47c17b8d
shivam-raj-4 <53818754+shivam-raj-4@users.noreply.github.com>
2019-11-20 13:08:54 +0530

    Add files via upload

---
+++ lab10a.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+void dijkstra();
+int c[10][10],n ,src;
+int main()
+{
+	int i,j;
+	printf("Enter the no. of vertices:\t");
+	scanf("%d",&n);
+	printf("\nEnter the cost matrix:\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+			scanf("%d",&c[i][j]);
+	}
+	printf("\n Enter the source node:\t");
+	scanf("%d",&src);
+	dijkstra();
+	return 0;
+}
+void dijkstra()
+{
+	int vis[10],dist[10],u,j,count,min;
+	for(j=1;j<=n;j++)
+		dist[j]=c[src][j];
+	for(j=1;j<=n;j++)
+		vis[j]=0;
+	dist[src]=0;
+	vis[src]=1;
+	count=1;
+	while(count!=n)
+	{
+		min=999;
+		for(j=1;j<=n;j++)
+		{
+			if(dist[j]<min && vis[j]!=1)
+			{
+				min =dist[j];
+				u=j;
+			}
+		}
+		vis[u]=1;
+		count++;
+		for(j=1;j<=n;j++)
+		{
+
+			if(min+c[u][j]<dist[j]&&vis[j]!=1)
+				dist[j]=min+c[u][j];
+		}
+	}
+	printf("\n The shortest distance is :\n");
+	for(j=1;j<=n;j++)
+		printf("\n%d-->%d=%d",src,j,dist[j]);
+} 
